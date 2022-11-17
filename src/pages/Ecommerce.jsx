f18f3d56c6aed10b44196a6caded23acb19d279b
Agustin <hannheitiragustin@hotmail.com>
2022-11-17 19:50:56 -0300

    update: Stacked.jsx finalizado

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -118,7 +118,7 @@ const Ecommerce = () => {
               </div>
             </div>
             <div>
-              <Stacked width="320px" heigth="360px" />
+              <Stacked width="320px" height="360px" />
             </div>
           </div>
         </div>
