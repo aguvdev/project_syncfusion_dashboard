ddf47a2d45c4a74cfb0194d3bb1f2070cf025ab8
Agustin <hannheitiragustin@hotmail.com>
2022-11-17 20:07:50 -0300

    add: header component terminado

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnDirective, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from "@syncfusion/ej2-react-grids";
+
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components"
+
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header category="Page" title="Orders" />
+    </div>
   )
 }
 
