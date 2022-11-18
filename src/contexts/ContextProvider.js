ee153c03243fb09d51f3a61ce000f73abe114e6b
Agustin <hannheitiragustin@hotmail.com>
2022-11-18 01:30:07 -0300

    update: sidebar theme seleccion de tema y color

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -21,12 +21,16 @@ export const ContextProvider = ({ children }) => {
     setCurrentMode(e.target.value);
 
     localStorage.setItem('themeMode', e.target.value);
+
+    setThemeSettings(false);
   }
+  
+  const setColor = (color) => {
+    setCurrentColor(color);
+    
+    localStorage.setItem('colorMode', color);
 
-  const setColor = (e) => {
-    setCurrentColor(e.target.value);
-
-    localStorage.setItem('colorMode', e.target.value);
+    setThemeSettings(false);
   }
 
   const handleClick = (clicked) => {
@@ -46,12 +50,10 @@ export const ContextProvider = ({ children }) => {
         setScreenSize,
         currentColor,
         currentMode,
-        setCurrentColor,
-        setCurrentMode,
-        setColor,
-        setMode,
         themeSettings,
-        setThemeSettings
+        setThemeSettings,
+        setColor,
+        setMode
       }}
     >
       {children}
